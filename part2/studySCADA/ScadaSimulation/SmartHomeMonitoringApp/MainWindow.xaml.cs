5b3088c33deb4dd714e5897c754bba555aca3f78
CodingNewbie0 <ahrgjdy@gmail.com>
2023-05-08 12:45:26 +0900

    5월 8일

--- part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/MainWindow.xaml.cs
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using SmartHomeMonitoringApp.Views;
 
 namespace SmartHomeMonitoringApp
 {
@@ -29,14 +31,29 @@ namespace SmartHomeMonitoringApp
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
             // <Frame> ==> Page.xaml
-            // <ContentControl> ==> UserControl.xaml
-            ActiveItem.Content = new Views.DataBaseControl();
+            // <ContentControl> ==> UserControl.xaml 
+            //ActiveItem.Content = new Views.DataBaseControl();
         }
 
+        // 끝내기 버튼 클릭이벤트 핸들러
         private void MnuExitProgram_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.GetCurrentProcess().Kill(); // 완전한 프로그램 종료
-            Environment.Exit(0); // 둘중 하나만 씀
+            Process.GetCurrentProcess().Kill();  // 작업관리자에서 프로세스 종료!
+            Environment.Exit(0); // 둘중하나만 쓰면 됨
+        }
+
+        // MQTT 시작메뉴 클릭이벤트 핸들러
+        private void MnuStartSubscribe_Click(object sender, RoutedEventArgs e)
+        {
+            var mqttPopWin = new MqttPopupWindow();
+            mqttPopWin.Owner = this;
+            mqttPopWin.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            var result = mqttPopWin.ShowDialog();
+
+            if (result == true)
+            {
+                ActiveItem.Content = new Views.DataBaseControl();
+            }
         }
     }
 }
